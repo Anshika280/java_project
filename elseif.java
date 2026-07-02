39fd21e9cf21de2e8f61dd8d3eff8f8512b99a7c
Anshika Saxena <saxenaanshika280@gmail.com>
2026-07-02 15:13:14 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class elseif {
+    public static void main(int[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       
+    }
+}
